d9e3f52875867ee93ea1e8a6289d60e123fc6369
Rishu Kumar <126085066+rishu-7549@users.noreply.github.com>
2025-02-19 11:12:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 // complete this js code
-function Person(name, age) {}
+class Person{
+	Person(name,age){
+		this.name = name;
+		this.age = age;
+	}
+	greet(){
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+	}
+}
 
-function Employee(name, age, jobTitle) {}
+class Employee extends Person{
+	Employee(name,age,jobTitle){
+		super();
+		this.jobTitle = jobTitle;
+	}
+	jobGreet(){
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
+	}
+}
+// function Person(name, age) {}
+
+// function Employee(name, age, jobTitle) {}
 
 // Do not change code below this line
 window.Person = Person;
